9bc0af5c9f01f3be2d422fbdf0fb35b61d93d3ce
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-03-23 16:42:15 +0100

    dacker build/run

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mvn clean package -DskipTests
-docker build --build-arg JAR_FILE=target/pim-api-0.0.1-SNAPSHOT.jar -t abderrazak/pim-service:0.0.1-SNAPSHOT .
+docker build --build-arg JAR_FILE=target/pim-api-0.0.1-SNAPSHOT.jar -t abderrazak/pim-service:0.0.1-snapshot .
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -p 8080:8080 -ti abderrazak/pim-service:0.0.1-snapshot
